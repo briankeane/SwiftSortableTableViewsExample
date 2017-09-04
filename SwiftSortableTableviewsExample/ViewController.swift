1f8efc6b7f7f515666984a69ed12e007b9bbe8df
briankeane <lonesomewhistle@gmail.com>
2017-09-04 14:19:39 -0500

    basic uiTableView functionality working

--- SwiftSortableTableviewsExample/ViewController.swift
+++ SwiftSortableTableviewsExample/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import SwiftSortableTableviews
 
-class ViewController: UIViewController {
+let kCustomCellIdentifier = "kCustomCellIdentifier"
 
+class ViewController: UIViewController,SortableTableViewDelegate, SortableTableViewDataSource  {
+
+    @IBOutlet weak var numbersTableView: SortableTableView!
+
+    var numbersArray:Array<Int> = [0,1,2,3,4,5,6,7,8,9]
+    var lettersArray:Array<String> = ["a","b","c","d","e","f","g","h","i","j"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.numbersTableView.sortableDelegate = self
+        self.numbersTableView.sortableDataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    
+    
+    //------------------------------------------------------------------------------
+    
+    //                    Delegate/DataSource
+    //------------------------------------------------------------------------------
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+            let cell = self.numbersTableView.dequeueReusableCell(withIdentifier: kCustomCellIdentifier, for: indexPath) as! CustomCell
+            cell.label.text = String(numbersArray[indexPath.row])
+            return cell
+    }
+        
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return self.numbersArray.count
+    }
+    
+        
+    
+    
+    
+    
 }
 
